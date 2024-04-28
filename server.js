39d4c1dbc3c4478c1b2736412dcefcb488d3d606
Aditya kumar <83528896+Adityakr3@users.noreply.github.com>
2024-04-28 19:05:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -46,4 +46,4 @@ app.get('/bookings/:id', async (req, res) => {
 });
 
 
-app.listen(process.env.PORT, () => console.log('Server listening on port 3000'));
+app.listen(process.env.PORT ||3000 , () => console.log('Server listening on port 3000'));
