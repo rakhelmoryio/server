395dcdc8ac21527d12e8a379d0c6640cddb35d10
rakhelmoryio <168238218+rakhelmoryio@users.noreply.github.com>
2024-05-02 13:56:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -45,5 +45,39 @@ app.get('/bookings/:id', async (req, res) => {
   }
 });
 
+app.get('/update/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const booking = await Booking.findOne({ Booking_Id: id });
+    if (!booking) {
+      return res.status(404).json({ error: 'Booking not found' });
+    }
+    res.status(200).json(booking);
+  } catch (error) {
+    console.error('Error fetching booking:', error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+
+app.put('/update/:id', async (req, res) => {
+  const id = req.params.id;
+  const newData = req.body;
+
+  try {
+    const updatedBooking = await Booking.findOneAndUpdate(
+      { Booking_Id: id },
+      newData,
+      { new: true }
+    );
+    if (!updatedBooking) {
+      return res.status(404).json({ error: 'Booking not found' });
+    }
+    res.status(200).json(updatedBooking);
+  } catch (error) {
+    console.error('Error updating booking:', error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
 
 app.listen( PORT , () => console.log('Server listening'));
